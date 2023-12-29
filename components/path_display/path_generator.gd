ae285416738753d98de5032767457b7ce0346661
Marcos Fernández <marcos.fersu@gmail.com>
2023-12-29 07:12:51 -0300

    add lopp and enemy. autoload

--- components/path_display/path_generator.gd
+++ components/path_display/path_generator.gd
@@ -1,82 +1,98 @@
-extends Object
+extends Node
 class_name PathGenerator
 
-var _grid_length: int
-var _grid_height: int
 var _loop_count: int
 
-var _path: Array[Vector2i]
 
-func _init(length: int, height: int):
-	_grid_length = length
-	_grid_height = height
-	
+var path_config:PathGeneratorConfig = preload("res://resources/basic_path_conf.tres")
+
+var _path_route: Array[Vector2i]
+
+func _init():
+	generate_path(path_config.add_loops)
+
+	if path_config.add_loops:
+		while(_path_route.size() < path_config.min_path_size or _path_route.size() > path_config.max_path_size
+			or _loop_count < path_config.min_loops or _loop_count > path_config.max_loops
+		):
+				generate_path(path_config.add_loops)
+	else:
+		while(_path_route.size() < 35):
+			generate_path(path_config.add_loops)
+			
+			
 func generate_path(add_loops:bool = false):
-	_path.clear()
-	_loop_count = 0
+	_path_route.clear()
 	randomize()
+	_loop_count = 0
 	
-	var x: int = 0
-	var y: int = int(_grid_height/2)
+	var x = 0
+	var y = int(path_config.map_height/2.0)
 	
-	while x < _grid_length:
-		if not _path.has(Vector2i(x, y)):
-			_path.append(Vector2i(x, y))
-			
-		var choice: int = randi_range(0, 2)
+	while x < path_config.map_length:
+		if not _path_route.has(Vector2i(x,y)):
+			_path_route.append(Vector2i(x,y))
 		
-		if choice == 0 or x % 2 == 0 or x == _grid_length - 1: 
+		var choice:int = randi_range(0,2)
+
+		if choice == 0 or x < 2 or x % 2 == 0 or x == path_config.map_length-1:
 			x += 1
-		elif choice == 1 and y < _grid_height - 2  and not _path.has(Vector2i(x, y + 1)):
+		elif choice == 1 and y < path_config.map_height-2 and not _path_route.has(Vector2i(x,y+1)):
 			y += 1
-		elif choice == 2 and y > 2 and not _path.has(Vector2i(x, y - 1)):
+		elif choice == 2 and y > 1 and not _path_route.has(Vector2i(x,y-1)):
 			y -= 1
-			
+	
 	if add_loops:
 		_add_loops()
+
 		
-	return _path
+	return _path_route
+
 
 func get_tile_score(index: int) -> int:
 	var score: int = 0
+	var x = _path_route[index].x
+	var y = _path_route[index].y
 	
-	var x = _path[index].x
-	var y = _path[index].y
-
-	score += 1 if _path.has(Vector2i(x, y - 1)) else 0
-	score += 2 if _path.has(Vector2i(x + 1, y)) else 0
-	score += 4 if _path.has(Vector2i(x, y + 1)) else 0
-	score += 8 if _path.has(Vector2i(x - 1, y)) else 0
+	score += 1 if _path_route.has(Vector2i(x,y-1)) else 0
+	score += 2 if _path_route.has(Vector2i(x+1,y)) else 0
+	score += 4 if _path_route.has(Vector2i(x,y+1)) else 0
+	score += 8 if _path_route.has(Vector2i(x-1,y)) else 0
 	
 	return score
 
-func get_path() -> Array[Vector2i]:
-	return _path
+
+func get_path_route() -> Array[Vector2i]:
+	return _path_route
+
+
+func get_path_tile(index: int) -> Vector2i:
+	return _path_route[index]
+
 
 func _add_loops():
-	# See if we can add any loops
-	var loops_generated:bool = true
+
+	var loops_generated: bool = true
 	
-	# Keep generating loops until you can't any more!
-	while loops_generated:
+
+	while loops_generated and _loop_count < path_config.max_loops:
 		loops_generated = false
-		for i in range(_path.size()):
-			var loop:Array[Vector2i] = _is_loop_option(i)
-			# If the loops size > 0, then _is_loop_option found a loop... So add
-			# it to the array!
+		for i in range(_path_route.size()):
+			var loop: Array[Vector2i] = _is_loop_option(i)
+
 			if loop.size() > 0:
 				loops_generated = true
 				for j in range(loop.size()):
-					_path.insert(i+1+j, loop[j])
+					_path_route.insert(i+1+j, loop[j])
 
 
 func _is_loop_option(index: int) -> Array[Vector2i]:
-	var x: int = _path[index].x
-	var y: int = _path[index].y
-	var return_path:Array[Vector2i]
+	var x: int = _path_route[index].x
+	var y: int = _path_route[index].y
+	var return_path: Array[Vector2i]
 
-	#Yellow
-	if (x < _grid_length-1 and y > 1
+
+	if (x < path_config.map_length-1 and y > 1
 		and _tile_loc_free(x, y-3) and _tile_loc_free(x+1, y-3) and _tile_loc_free(x+2, y-3)		
 		and _tile_loc_free(x-1, y-2) and _tile_loc_free(x, y-2) and _tile_loc_free(x+1, y-2) and _tile_loc_free(x+2, y-2) and _tile_loc_free(x+3, y-2)
 		and _tile_loc_free(x-1, y-1) and _tile_loc_free(x, y-1) and _tile_loc_free(x+1, y-1) and _tile_loc_free(x+2, y-1) and _tile_loc_free(x+3, y-1)
@@ -84,12 +100,12 @@ func _is_loop_option(index: int) -> Array[Vector2i]:
 		and _tile_loc_free(x+1,y+1) and _tile_loc_free(x+2,y+1)):
 		return_path = [Vector2i(x+1,y), Vector2i(x+2,y), Vector2i(x+2,y-1), Vector2i(x+2,y-2), Vector2i(x+1,y-2), Vector2i(x,y-2), Vector2i(x,y-1)]
 
-		if _path[index-1].y > y:
+		if _path_route[index-1].y > y:
 			return_path.reverse()
 			
 		_loop_count += 1
 		return_path.append(Vector2i(x,y))
-	#Blue
+
 	elif (x > 2 and y > 1
 			and _tile_loc_free(x, y-3) and _tile_loc_free(x-1, y-3) and _tile_loc_free(x-2, y-3)		
 			and _tile_loc_free(x-1, y) and _tile_loc_free(x-2, y) and _tile_loc_free(x-3, y)
@@ -98,13 +114,13 @@ func _is_loop_option(index: int) -> Array[Vector2i]:
 			and _tile_loc_free(x-1, y+1) and _tile_loc_free(x-2, y+1)):
 		return_path = [Vector2i(x,y-1), Vector2i(x,y-2), Vector2i(x-1,y-2), Vector2i(x-2,y-2), Vector2i(x-2,y-1), Vector2i(x-2,y), Vector2i(x-1,y)]
 
-		if _path[index-1].x > x:
+		if _path_route[index-1].x > x:
 			return_path.reverse()
 
 		_loop_count += 1
 		return_path.append(Vector2i(x,y))
-	#Red
-	elif (x < _grid_length-1 and y < _grid_height-2
+
+	elif (x < path_config.map_length-1 and y < path_config.map_height-2
 			and _tile_loc_free(x, y+3) and _tile_loc_free(x+1, y+3) and _tile_loc_free(x+2, y+3)		
 			and _tile_loc_free(x+1, y-1) and _tile_loc_free(x+2, y-1)
 			and _tile_loc_free(x+1, y) and _tile_loc_free(x+2, y) and _tile_loc_free(x+3, y)
@@ -112,13 +128,13 @@ func _is_loop_option(index: int) -> Array[Vector2i]:
 			and _tile_loc_free(x-1, y+2) and _tile_loc_free(x, y+2) and _tile_loc_free(x+1, y+2) and _tile_loc_free(x+2, y+2) and _tile_loc_free(x+3, y+2)):
 		return_path = [Vector2i(x+1,y), Vector2i(x+2,y), Vector2i(x+2,y+1), Vector2i(x+2,y+2), Vector2i(x+1,y+2), Vector2i(x,y+2), Vector2i(x,y+1)]
 
-		if _path[index-1].y < y:
+		if _path_route[index-1].y < y:
 			return_path.reverse()
 		
 		_loop_count += 1
 		return_path.append(Vector2i(x,y))
-	# Brown
-	elif (x > 2 and y < _grid_height-2
+
+	elif (x > 2 and y < path_config.map_height-2
 			and _tile_loc_free(x, y+3) and _tile_loc_free(x-1, y+3) and _tile_loc_free(x-2, y+3)
 			and _tile_loc_free(x-1, y-1) and _tile_loc_free(x-2, y-1)
 			and _tile_loc_free(x-1, y) and _tile_loc_free(x-2, y) and _tile_loc_free(x-3, y)
@@ -126,7 +142,7 @@ func _is_loop_option(index: int) -> Array[Vector2i]:
 			and _tile_loc_free(x+1, y+2) and _tile_loc_free(x, y+2) and _tile_loc_free(x-1, y+2) and _tile_loc_free(x-2, y+2) and _tile_loc_free(x-3, y+2)):
 		return_path = [Vector2i(x,y+1), Vector2i(x,y+2), Vector2i(x-1,y+2), Vector2i(x-2,y+2), Vector2i(x-2,y+1), Vector2i(x-2,y), Vector2i(x-1,y)]
 
-		if _path[index-1].x > x:
+		if _path_route[index-1].x > x:
 			return_path.reverse()
 		
 		_loop_count += 1
@@ -134,18 +150,26 @@ func _is_loop_option(index: int) -> Array[Vector2i]:
 		
 	return return_path
 	
-## Returns true if there is a path tile at the x,y coordinate.
 func _tile_loc_taken(x: int, y: int) -> bool:
-	return _path.has(Vector2i(x,y))
+	return _path_route.has(Vector2i(x,y))
 	
-## Returns true if there is no path tile at the x,y coordinate.
 func _tile_loc_free(x: int, y: int) -> bool:
-	return not _path.has(Vector2i(x,y))
+	return not _path_route.has(Vector2i(x,y))
 
-## Returns the number of loops currently in the path.
 func get_loop_count() -> int:
 	return _loop_count
 
-## Returns the Vector2i path tile at the given index.
-func get_path_tile(index: int) -> Vector2i:
-	return _path[index]
+#func to_path_follow_3d() -> PathFollow3D:
+#	var c3d:Curve3D = Curve3D.new()
+#
+#	for element in _path:
+#		c3d.add_point(Vector3(element.x, 0.4, element.y))
+#
+#	var p3d:Path3D = Path3D.new()
+##	add_child(p3d)
+#	p3d.curve = c3d
+#
+#	var pf3d:PathFollow3D = PathFollow3D.new()
+#	p3d.add_child(pf3d)
+#
+#	return pf3d
